af3a2c6632ec86dc28ffe416264a75f6912acf1e
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-02-27 20:01:54 +0100

    Create 112.py

---
+++ D3/112.py
@@ -0,0 +1,38 @@
+# Possible solution:
+a = float(input("Number 1: "))
+op = input("Operation: ")
+b = float(input("Number 2: "))
+while a != 0 or op != '+' or b != 0:
+    if op == '+':
+        result = a + b
+    elif op == '-':
+        result = a - b
+    elif op == '*':
+        result = a * b
+    elif op == '/':
+        result = a / b
+    else:
+        print(f"Invalid operation '{op}'. Try again.")
+    print(f"{a} {op} {b} = {result}")
+    a = float(input("Number 1: "))
+    op = input("Operation: ")
+    b = float(input("Number 2: "))
+
+# or using `while True` with break:
+while True:
+    a = float(input("Number 1: "))
+    op = input("Operation: ")
+    b = float(input("Number 2: "))
+    if a == 0 and op == '+' and b == 0:
+        break
+    if op == '+':
+        result = a + b
+    elif op == '-':
+        result = a - b
+    elif op == '*':
+        result = a * b
+    elif op == '/':
+        result = a / b
+    else:
+        print(f"Invalid operation '{op}'. Try again.")
+    print(f"{a} {op} {b} = {result}")
