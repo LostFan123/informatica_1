aea40ccc59dbc9576398c487118939f561c21db2
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-03 18:41:52 +0100

    Update 110.py

--- D2/110.py
+++ D2/110.py
@@ -5,7 +5,7 @@ c = int(input("Number 3: "))
 d = int(input("Number 4: "))
 odd_numbers_count = 0
 if a % 2 != 0:
-    odd_numbers_count += 1
+    odd_numbers_count += 1  # this is called "augmented assignment" and it's the same as odd_numbers_count = odd_numbers_count + 1
 if b % 2 != 0:
     odd_numbers_count += 1
 if c % 2 != 0:
