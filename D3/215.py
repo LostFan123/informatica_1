a23eb34d8f18e49763d6aee4e91c2630384f684e
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-05 12:12:22 +0100

    Update 215.py

--- D3/215.py
+++ D3/215.py
@@ -10,7 +10,23 @@ while counter < 21:
     else:
         print(counter)
     counter += 1
-
+    
+# 2. Performing the same operations is not optimal.
+counter = 1
+while counter < 101:
+    if counter % 3 == 0 and counter % 5 != 0:
+        print("Fizz")
+        counter += 1
+    if counter % 3 != 0 and counter % 5 == 0:
+        print("Buzz")
+        counter += 1
+    if counter % 3 == 0 and counter % 5 == 0:
+        print("FizzBuzz")
+        counter += 1
+    else:
+        print(counter)
+        counter += 1
+    
 # Possible solution:
 number = 1
 while number < 101:
