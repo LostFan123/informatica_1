829b28441ab711989b7723f2e55b0a9fb4473a5a
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-05 17:41:53 +0100

    Update 112.py

--- D3/112.py
+++ D3/112.py
@@ -1,3 +1,28 @@
+# 1. Incorrect check for the "exit" condition can cause problems.
+#    Failing example: 2 + (-2)
+status = True
+result = 0
+left_operand = 0
+right_operand = 0
+operation = ''
+while status:
+    operation = input("Operation: ")
+    left_operand = int(input("Enter a number: "))
+    right_operand = int(input("Entry a second number: "))
+    if operation == '+':
+        result = left_operand + right_operand
+    elif operation == '-':
+        result = left_operand - right_operand
+    elif operation == '*':
+        result = left_operand * right_operand
+    elif operation == '/':
+        result = left_operand / right_operand
+    else:
+        print("You haven't selected an operation")
+    print(f"The result of the operation is {result}.")
+    status = False if result == 0 and operation == '+' else True
+print('Done!')
+
 # Possible solution:
 # Note: we use `elif`s instead of `if`s as we don't need to check the inputted symbol against all possible valid symbols
 a = float(input("Number 1: "))
