cc186a9bf1379e2e576b0f95859d2f1fedf943c3
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-05 15:31:31 +0100

    Update 215.py

--- D3/215.py
+++ D3/215.py
@@ -1,6 +1,6 @@
 # 1. Using several if's instead of if-elif will make it check the same number several times
 counter = 1
-while counter < 21:
+while counter < 101:
     if counter % 3 == 0:
         print("Fizz")
     if counter % 5 == 0:
@@ -39,3 +39,17 @@ while number < 101:
     else:
         print(number)
     number += 1
+
+# Alternative based on concatenation of strings:
+number = 1
+while number < 101:
+    result = ""
+    if number % 3 == 0:
+        result += "Fizz"
+    if number % 5 == 0:
+        result += "Buzz"
+    if len(result) == 0:
+        result = number
+    print(result)
+    number += 1
+    
