690b715a16a9c1fa3979c04847b3084ab82f9943
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-05 11:29:01 +0100

    Update 21.py

--- D3/21.py
+++ D3/21.py
@@ -1,4 +1,14 @@
-# Possible solution. Using a flag variable to stop the loop.
+# 1. We don't need to continue looping if we already found out that the number is not a prime.
+#    Failing example: 12
+number = int(input("Number: "))
+divisor = number - 1
+while divisor > 1:
+    if number % divisor == 0:
+        print(f"{number} is not a prime")  # <- If we reach here, the loop should stop
+    divisor -= 1
+print(f"{number} is prime")  # <- This will be executed always. Not what we want!
+
+# Possible solution. Using a **flag variable** to stop the loop.
 number = int(input("Number: "))
 divisor = 2
 divisor_is_found = False
