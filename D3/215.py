d6d7cfc7360d04161b02965c628a6c34bc3fac78
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-05 10:58:32 +0100

    Update 215.py

--- D3/215.py
+++ D3/215.py
@@ -1,5 +1,19 @@
+# 1. Using several if's instead of if-elif will make it check the same number several times
+counter = 1
+while counter < 21:
+    if counter % 3 == 0:
+        print("Fizz")
+    if counter % 5 == 0:
+        print("Buzz")
+    if counter % 3 == 0 and counter % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(counter)
+    counter += 1
+
+# Possible solution:
 number = 1
-while number != 101:
+while number < 101:
     if number % 15 == 0:
         print("FizzBuzz")
     elif number % 3 == 0:
