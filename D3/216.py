1f7637a95121925ba0936faa2db8415c2e6113bf
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-05 11:09:04 +0100

    Update 216.py

--- D3/216.py
+++ D3/216.py
@@ -1,11 +1,22 @@
-# possible solution without using temporary variables
+# Possible solution:
 first = 1
-second = 2
-counter = 2
 print(first)
-print(second)
-while counter != 20:
+second = 2
+counter = 1
+while counter < 20:
+    print(second)
+    temp = first + second
+    first = second
+    second = temp
+    counter += 1
+
+# Improvement: temporary variable can be avoided
+first = 1
+print(first)
+second = 2
+counter = 1
+while counter < 20:
+    print(second)
     second = first + second
     first = second - first
-    print(second)
     counter += 1
