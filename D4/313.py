45f913a66684afd289204dcd05ea7ea3ca39ff17
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-06 17:18:45 +0100

    Create 313.py

---
+++ D4/313.py
@@ -0,0 +1,28 @@
+# 1. Logical mistake. All digits != any digits:
+number = int(input("Number: "))
+even_digits_count = 0
+while number != 0:
+    digit = number % 10
+    number = number // 10
+    print(digit)
+    if digit % 2 == 0:
+        even_digits_count += 1
+    elif digit == 0:
+        even_digits_count += 1
+print("Number of even digits:", even_digits_count)
+if even_digits_count != 0:
+    print("All digits are even.")
+else:
+    print("No digits are even.")
+    
+# Possible solution:
+number = int(input("Number: "))
+all_digits_are_even = True
+while number != 0 and all_digits_are_even:
+    number, digit = divmod(number, 10)
+    if digit % 2 != 0:
+        all_digits_are_even = False
+if all_digits_are_even != 0:
+    print("All digits are even.")
+else:
+    print("Not all digits are even.")
