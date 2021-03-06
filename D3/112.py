2ffd6a1998605a6d914321ba310373508d8a2cfb
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-06 11:31:09 +0100

    Update 112.py

--- D3/112.py
+++ D3/112.py
@@ -1,5 +1,6 @@
 # 1. Incorrect check for the "exit" condition can cause problems.
 #    Failing example: 2 + (-2)
+# 2. Unnecessary initialization of some variables before the loop.
 status = True
 result = 0
 left_operand = 0
@@ -18,7 +19,7 @@ while status:
     elif operation == '/':
         result = left_operand / right_operand
     else:
-        print("You haven't selected an operation")
+        print("You haven't selected an operation")  # here we should also stop the current iteration
     print(f"The result of the operation is {result}.")
     status = False if result == 0 and operation == '+' else True
 print('Done!')
@@ -37,8 +38,6 @@ while a != 0 or op != '+' or b != 0:  # same as `while not (a == 0 and op == '+'
         result = a * b
     elif op == '/':
         result = a / b
-    else:
-        print(f"Invalid operation '{op}'. Try again.")
     print(f"{a} {op} {b} = {result}")
     a = float(input("Number 1: "))
     op = input("Operation: ")
@@ -62,3 +61,26 @@ while True:
     else:
         print(f"Invalid operation '{op}'. Try again.")
     print(f"{a} {op} {b} = {result}")
+
+# One of the solutions I've received used string comparisons:
+print("To exit the program you must write the following operation : 0+0 .")
+n1 = int(input("Enter the first number: "))
+op = str(input("Enter operation ( + , - , / , * ) : "))
+n2 = int(input("Enter the second number: "))
+total = str(n1) + op + str(n2)
+while total != "0+0":
+	if op == "+":
+		print("The result is " , n1 + n2 , ".")
+	elif op == "-":
+		print("The result is " , n1 - n2 , ".")
+	elif op == "/":
+		print("The result is " , n1 / n2 , ".")
+	elif op == "*":
+		print("The result is " , n1 * n2 , ".")
+	else:
+		print("The operation must be + or - or * or /.")
+	n1 = int(input("Enter another number: "))
+	op = str(input("Enter operation ( + , - , / , * ) : "))
+	n2 = int(input("Enter another number: "))
+	total = str(n1) + op + str(n2) 
+# a simpler way to write this condtion will be `while (n1, op, n2) != (0, '+', 0):`
