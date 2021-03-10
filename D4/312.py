c8e1acbf5ada3a550a91bb1d0d6ca5cddee37d62
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-10 18:38:17 +0100

    Update 312.py

--- D4/312.py
+++ D4/312.py
@@ -1,3 +1,22 @@
+# 1. No need to count odd digits.
+# 2. Incorrect order of calculating quotient and remainder - n gets overwritten before we get the first digit.
+#    Failing example: 21
+n = int(input("Tell me a number: "))
+even = 0
+odd = 0
+while (n>0):
+    n = n//10
+    if ((n%10)%2 == 0):
+        even = even + 1
+    else: 
+        odd = odd + 1
+if (even == 2):
+    print("This number has 2 even digits")
+elif (even > 2):
+    print("This number has more than 2 even digits")
+else:
+    print("This number has no even digits")
+
 # Good solution, but avoid repeating logic 
 number = int(input("Enter a number: "))
 counter = 0
