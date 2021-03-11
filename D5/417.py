2a99459a5cb24b954f253721ef007e3c2452f2b6
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-11 17:07:25 +0100

    Create 417.py

---
+++ D5/417.py
@@ -0,0 +1,13 @@
+# 1. Using list.index() inside a loop is not optimal 
+#    since under the hood it will perform unnecessary search for the value:
+values = [4, 8, 15, 16, 23, 42]
+for value in values:
+    if value % 3 == 0:
+        values[values.index(value)] == 0
+print(values)
+
+# Possible solution - iterate over indices:
+for index in range(len(values)):
+    if values[index] % 3 == 0:
+        values[index] = 0
+print(values)
