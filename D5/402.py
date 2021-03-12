80a74db8f77d029550f37fce2af38ca59dd0e48c
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-12 10:17:45 +0100

    Update 402.py

--- D5/402.py
+++ D5/402.py
@@ -1,3 +1,18 @@
+# 1. This solution violates DRY (Don't repeat yourself) principle. 
+#    It is not easily extensible and errorprone. Use loops for these tasks.
+v1 = input("Introduce a value:")
+v2 = input("Introduce a value:")
+v3 = input("Introduce a value:")
+v4 = input("Introduce a value:")
+v5 = input("Introduce a value:")
+v6 = input("Introduce a value:")
+v7 = input("Introduce a value:")
+v8 = input("Introduce a value:")
+v9 = input("Introduce a value:")
+v10 = input("Introduce a value:")
+V1 = [v1, v2, v3, v4, v5, v6, v7, v8, v8, v9, v10]
+print(V1)
+
 # Possible solution using while loop:
 values = [0] * 10
 index = 0
