eac29bc21a81e4838a62ece50827abd22379e78e
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-12 18:06:17 +0100

    Update 111.py

--- D2/111.py
+++ D2/111.py
@@ -1,24 +1,20 @@
-# 1. Check the logic with simple test cases: 1, 4, 100, 400.
-# The following will fail for 100:
-year = int(input("Year: "))
-if year % 4 == 0 or year % 400 == 0 and year % 100 != 0:
-    print("Leap year")
-else:
-    print("Not leap year")
+# Task:
+#   Write a program that reads from keyboard one year number and indicates if that year is or not
+#   a leap year (bisiesto). NOTE: Leap years are those that are multiple of 4 but not multiples of
+#   100, with the exception of the multiples of 400 which are leap years.
 
-    
+# ------------------------------------------------------------------------------------------------
 # Possible solution:
-year = int(input("Enter a year: "))
-if (year % 4) == 0 and not (year % 100) == 0:
-    print(str(year) + " is a leap year. ")
-elif year % 400 == 0:
-    print(str(year) + " is a leap year. ")
-else:
-    print(str(year) + " is not a leap year. ")
-    
-# or without `elif`:
 year = int(input("Enter a year: "))
 if year % 400 == 0 or year % 4 == 0 and not year % 100 == 0:
     print(str(year) + " is a leap year. ")
 else:
     print(str(year) + " is not a leap year. ")
+    
+
+# ------------------------------------------------------------------------------------------------
+# Advanced solution:
+#   Python already has a built-in functionality for this task.
+import calendar
+year = int(input("Enter a year: "))
+print(calendar.isleap(year))
