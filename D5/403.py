6ab9989afa7aaa9e09bdd384197344e5292c0cc3
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-12 08:59:08 +0100

    Create 403.py

---
+++ D5/403.py
@@ -0,0 +1,11 @@
+# Possible solution using while loop:
+values = [4, 8, 15, 16, 23, 42]
+index = 0
+while index < len(values):
+    print(index, values[index])
+    index += 1
+ 
+# Possible solution using for loop:
+values = [4, 8, 15, 16, 23, 42]
+for index in range(len(values)):
+    print(index, values[index])
