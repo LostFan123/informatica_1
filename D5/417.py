e1ed77bfbebd191483b2765d14ed8e1b60773461
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-12 11:03:25 +0100

    Update 417.py

--- D5/417.py
+++ D5/417.py
@@ -6,6 +6,13 @@ for value in values:
         values[values.index(value)] == 0
 print(values)
 
+# 2. Simply printing zeros won't do. The task asks us to update the list:
+values = [12, 3, 4, 65, 23, 81, 43, 9]
+for value in values:
+   if value % 3 == 0:
+       value = 0
+   print(value)
+
 # Possible solution - iterate over indices:
 for index in range(len(values)):
     if values[index] % 3 == 0:
