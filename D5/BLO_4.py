584157bddb9835b19ac36a4698a207067e2e8ad9
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-12 12:29:49 +0100

    Update BLO_4.py

--- D5/BLO_4.py
+++ D5/BLO_4.py
@@ -1,6 +1,23 @@
-# 1. Try to avoid "hardcoded" values. 
+# 1. Not the most optimal approach.
+#    It is not extensible - try adding more age ranges and corresponding calories values, and you will have to add more `elif` cases.
+#    Instead you can use for/while loops.
+A = [25, 40, 65, 80]
+KC = [120, 110, 105, 100, 90]
+age = int(input("Introduce your age: "))
+if age < A[0]:
+    print(str(KC[0]))
+elif age < A[1] and age >= A[0]:
+    print(str(KC[1]))
+elif age < A[2] and age >= A[1]:
+    print(str(KC[2]))
+elif age < A[3] and age >= A[2]:
+    print(str(KC[3]))
+else:
+    print(str(KC[4]))
+
+# 2. Try to avoid "hardcoded" values. 
 #    Here, 5 is hardcoded and could be replaced by `len(A) - 1`.
-# 2. The intent of the `i6 += 5` line can be confusing. 
+# 3. The intent of the `i6 += 5` line can be confusing. 
 #    Use flag variable or `break` statement instead.
 A = [25, 40, 65, 80]
 KC = [120, 110, 105, 100, 90]
