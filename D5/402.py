9b364c37903417252ee378ca4c32ba874d0a303c
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-13 13:36:53 +0100

    Update 402.py

--- D5/402.py
+++ D5/402.py
@@ -1,31 +1,32 @@
-# 1. This solution violates DRY (Don't repeat yourself) principle. 
-#    It is not easily extensible and errorprone. Use loops for these tasks.
-v1 = input("Introduce a value:")
-v2 = input("Introduce a value:")
-v3 = input("Introduce a value:")
-v4 = input("Introduce a value:")
-v5 = input("Introduce a value:")
-v6 = input("Introduce a value:")
-v7 = input("Introduce a value:")
-v8 = input("Introduce a value:")
-v9 = input("Introduce a value:")
-v10 = input("Introduce a value:")
-V1 = [v1, v2, v3, v4, v5, v6, v7, v8, v8, v9, v10]
-print(V1)
+# Task:
+#   Initialize vector V with 10 values read from keyboard
 
-# 2. The following will read only one number and repeat it 10 times.
-values = 10 * [int(input('Enter value: '))]
-
+# ------------------------------------------------------------------------------------------------
 # Possible solution using while loop:
 values = [0] * 10
 index = 0
 while index < len(values):
-    values[index] = int(input("Enter a number: "))
+    values[index] = input("Enter text: ")
     index += 1
 print(values)
 
 # Possible solution using for-loop:
 values = [0] * 10
 for index in range(len(values)):
-    values[index] = int(input("Enter a number: "))
+    values[index] = input("Enter text: ")
 print(values)
+
+# ------------------------------------------------------------------------------------------------
+# Advanced solutions:
+# In this case it is actually fine to use `append`:
+values = []
+for _ in range(10):
+    values.append(input("Enter text: "))
+    
+# A shorter way is to use "list comprehension":
+values = [input("Enter text: ") for _ in range(10)]
+
+# ------------------------------------------------------------------------------------------------
+# Common mistake:
+# 1. The following will read only one number and repeat it 10 times.
+values = 10 * [int(input('Enter value: '))]
