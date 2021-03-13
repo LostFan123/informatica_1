ada57f039be43fad529382d96f06337f8aead96d
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-13 12:46:15 +0100

    Update 312.py

--- D4/312.py
+++ D4/312.py
@@ -1,4 +1,48 @@
-# 1. No need to count odd digits.
+# Task:
+#   Determine if the number n has 2 or more even digits
+
+# ------------------------------------------------------------------------------------------------
+# Possible solution:
+number = int(input("Enter a number: "))
+counter = 0
+while counter < 2 and number != 0:
+    number, digit = divmod(number, 10)
+    if digit % 2 == 0:
+        counter = counter + 1
+if counter == 2:
+    print("There are at least 2 even digits")
+else:
+    print("There are no 2 even digits")
+    
+# ------------------------------------------------------------------------------------------------
+# Advanced solutions:
+#   Treating a number as a string and using the for-else loop construct.
+number = input("Enter a number: ")
+counter = 0
+for digit in number:
+    if int(digit) % 2 == 0:
+        counter = counter + 1
+    if counter == 2:
+        print("There are at least 2 even digits")
+        break
+else:
+    print("There are no 2 even digits")
+    
+# Alternatively we can use `sum` function with a generator expression.
+# To check if a digit is even we can make a set, https://docs.python.org/3/tutorial/datastructures.html#sets 
+# of all even digits in advance. 
+# We will also use the fact that we can add booelans together: `True + True == 2`
+even_digits = {'0', '2', '4', '6', '8'}
+number = input("Enter a number: ")
+even_digits_count = sum(digit in even_digits for digit in number)  # `digit in even_digits` will return `True` or `False`
+if even_digits_count >= 2:
+    print("There are at least 2 even digits")
+else:
+    print("There are no 2 even digits")
+    
+# ------------------------------------------------------------------------------------------------
+# Common mistales:
+# 1. Unnecessary counting of odd digits.
 # 2. Incorrect order of calculating quotient and remainder - n gets overwritten before we get the first digit.
 #    Failing example: 21
 n = int(input("Tell me a number: "))
@@ -17,17 +61,4 @@ elif (even > 2):
 else:
     print("This number has no even digits")
 
-# Good solution, but avoid repeating logic 
-number = int(input("Enter a number: "))
-counter = 0
-while counter < 2 and number != 0:
-    digit = number % 10
-    if digit % 2 == 0:
-        counter = counter + 1
-        number = number // 10  # this is repeated twice
-    else:
-        number = number // 10
-if counter == 2:
-    print("There are at least 2 even digits")
-else:
-    print("There are no 2 even digits")
+
