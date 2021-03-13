6227372fa3e5babd8e220f2bdc325fd5983090b2
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-13 11:46:21 +0100

    Update 204.py

--- D4/204.py
+++ D4/204.py
@@ -1,3 +1,11 @@
+# Task:
+#   Write a program that calculates the power xy
+#    for 2 numbers entered from keyboard using a
+#   loop of products since xy = x*x...*x, up to y-times. Before the loop you should verify that the value
+#   of y is positive, otherwise the program will stop..
+
+# ------------------------------------------------------------------------------------------------
+# Possible solution:
 number = float(input("Number: "))
 exponent = int(input("Exponent: "))
 if exponent < 0:
@@ -7,4 +15,14 @@ counter = 0
 while counter < exponent:
     result *= number
     counter += 1
+print(f"{number} ** {exponent} == {result}")
+
+# If for-loop is allowed:
+number = float(input("Number: "))
+exponent = int(input("Exponent: "))
+if exponent < 0:
+    raise ValueError("Exponent must be a positive integer") 
+result = 1
+for _ in range(exponent):
+    result *= number
 print(f"{number} ** {exponent} == {result}")
