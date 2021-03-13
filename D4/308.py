548287c35eb393c3d45dff8889d35b773e2fa643
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-13 12:32:46 +0100

    Update 308.py

--- D4/308.py
+++ D4/308.py
@@ -1,17 +1,21 @@
-# 1. Incorrectly understood task. We need a sequence of numbers, not digits:
-sequence=int(input("Number: "))
-count_of_even_digits=0
-while sequence!=0:
-  digit_=sequence%10
-  sequence=sequence//10
-  print(digit_)
-  if digit_%2==0:
-   count_of_even_digits+=1
-  elif digit_==0:
-   count_of_even_digits+=1
-print("This is the number of even digits introduced: ",count_of_even_digits)
+# Task:
+#   Determine if all the numbers of a sequence of numbers read from keyboard and finished in
+#   0 are even. 
 
+# ------------------------------------------------------------------------------------------------
 # Possible solution:
+are_all_even = True
+number = 1  # just some default value to enter the loop
+while number != 0:
+    number = int(input("N: "))
+    if number % 2 != 0:
+        are_all_even = False
+if are_all_even:
+    print("All numbers are even")
+else:
+    print("Not all numbers are even")
+
+# or using `while True` with `break`:
 are_all_even = True
 while True:
     number = int(input("N: "))
@@ -23,3 +27,5 @@ if are_all_even:
     print("All numbers are even")
 else:
     print("Not all numbers are even")
+
+
