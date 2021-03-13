59a2c340ffc57f768c89607695b429a6811038d0
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-13 11:53:14 +0100

    Update 206.py

--- D4/206.py
+++ D4/206.py
@@ -1,3 +1,10 @@
+# Task:
+#   Write a program that calculates the sum of all the divisors of a positive integer number X
+#   introduced from keyboard. Remember that Y is a divisor of X if Y<X and the remainder of the
+#   integer division is 0.
+
+# ------------------------------------------------------------------------------------------------
+# Possible solution:
 number = int(input("Number: "))
 divisor = 2
 divisors_sum = 0
@@ -6,4 +13,21 @@ while divisor <= number // 2:
         print("Divisor:", divisor)
         divisors_sum += divisor
     divisor += 1
+print("Sum of the divisors:", divisors_sum)
+
+# Or if for-loop is allowed:
+number = int(input("Number: "))
+divisors_sum = 0
+for divisor in range(2, number // 2 + 1):
+    if number % divisor == 0:
+        print("Divisor:", divisor)
+        divisors_sum += divisor
+print("Sum of the divisors:", divisors_sum)
+
+# ------------------------------------------------------------------------------------------------
+# Advanced solution:
+#   Using `sum` with a generator expression passed to it as an argument.
+number = int(input("Number: "))
+divisors_sum = sum(divisor for divisor in range(2, number // 2 + 1)
+                   if number % divisor == 0)
 print("Sum of the divisors:", divisors_sum)
