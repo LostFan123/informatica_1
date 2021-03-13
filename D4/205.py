889b11fda227a15e31c36674028d1cc9ae47653d
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-13 11:48:59 +0100

    Update 205.py

--- D4/205.py
+++ D4/205.py
@@ -1,6 +1,22 @@
+# Task:
+#   Write a program that counts the number of digits of a positive integer number X introduced
+#   from keyboard. Use a loop that divides X by 10 successively until 0. 
+
+# ------------------------------------------------------------------------------------------------
+# Solution:
 number = int(input("Number: "))
 digits_count = 0
 while number != 0:
+    number = number // 10
+    digits_count += 1
+print(digits_count)
+
+# Alternatively using `while True:` with `break`:
+number = int(input("Number: "))
+digits_count = 0
+while True:
+    if number == 0:  # or `if not number:`
+        break
     number = number // 10
     digits_count += 1
 print(digits_count)
