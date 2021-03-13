2038b1eb6096e5ef2f8182d123d0cb31b376a04e
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-13 18:10:02 +0100

    Update 312.py

--- D4/312.py
+++ D4/312.py
@@ -15,6 +15,27 @@ else:
     print("There are no 2 even digits")
     
 # ------------------------------------------------------------------------------------------------
+# Common mistales:
+# 1. Unnecessary counting of odd digits.
+# 2. Incorrect order of calculating quotient and remainder - n gets overwritten before we get the first digit.
+#    Failing example: 21
+n = int(input("Tell me a number: "))
+even = 0
+odd = 0
+while (n>0):
+    n = n//10
+    if ((n%10)%2 == 0):
+        even = even + 1
+    else: 
+        odd = odd + 1
+if (even == 2):
+    print("This number has 2 even digits")
+elif (even > 2):
+    print("This number has more than 2 even digits")
+else:
+    print("This number has no even digits")
+
+# ------------------------------------------------------------------------------------------------
 # Advanced solutions:
 #   Treating a number as a string and using the for-else loop construct.
 number = input("Enter a number: ")
@@ -39,26 +60,3 @@ if even_digits_count >= 2:
     print("There are at least 2 even digits")
 else:
     print("There are no 2 even digits")
-    
-# ------------------------------------------------------------------------------------------------
-# Common mistales:
-# 1. Unnecessary counting of odd digits.
-# 2. Incorrect order of calculating quotient and remainder - n gets overwritten before we get the first digit.
-#    Failing example: 21
-n = int(input("Tell me a number: "))
-even = 0
-odd = 0
-while (n>0):
-    n = n//10
-    if ((n%10)%2 == 0):
-        even = even + 1
-    else: 
-        odd = odd + 1
-if (even == 2):
-    print("This number has 2 even digits")
-elif (even > 2):
-    print("This number has more than 2 even digits")
-else:
-    print("This number has no even digits")
-
-
