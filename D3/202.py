4ebb1ff0b7e46c178a47ba239f9caf0f24dbece1
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-13 10:43:16 +0100

    Update 202.py

--- D3/202.py
+++ D3/202.py
@@ -1,17 +1,7 @@
-# 1. We have tools to avoid code duplication: 
-number1 = int(input("Enter the first number: "))
-number2 = int(input("Enter the second number: "))
-number3 = int(input("Enter the third number: "))
-number4 = int(input("Enter the fourth number: "))
-number5 = int(input("Enter the fifth number: "))
-number6 = int(input("Enter the sixth number: "))
-number7 = int(input("Enter the seventh number: "))
-number8 = int(input("Enter the eighth number: "))
-number9 = int(input("Enter the nineth number: "))
-number10 = int(input("Enter the tenth number: "))
-finalnumber=(number1+number2+number3+number4+number5+number6+number7+number8+number9+number10)
-print(finalnumber)
+# Task:
+#   Write a program that calculates the sum of 10 numbers entered from keyboard.
 
+# ------------------------------------------------------------------------------------------------
 # Possible solution:
 counter = 0
 total = 0
@@ -19,3 +9,14 @@ while counter != 10:
     total += float(input("Number: "))
     counter += 1
 print(total)
+
+# If for-loop is allowed:
+total = 0
+for _ in range(10):  # the underscore is a convention for "throwaway variables"; you can replace it by any other name you want, like `index`
+    total += float(input("Number: "))
+print(total)
+
+# ------------------------------------------------------------------------------------------------
+# Advanced solution:
+#   Using `sum` function with a generator expression passed to it as an argument:
+print(sum(float(input("Number: ")) for _ in range(10)))
