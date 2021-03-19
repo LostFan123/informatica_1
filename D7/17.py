a17cdec66e8fb7b86a4f45ace2f03684be3414c0
Georgy Skorobogatov <skorobogatov@phystech.edu>
2021-03-19 11:45:57 +0100

    Create 17.py

---
+++ D7/17.py
@@ -0,0 +1,15 @@
+# Task:
+#   Write a program that reads from keyboard three numbers and determines if the following
+#   property meets: one of them is equal to the sum of the other two. For instance numbers 7, 21 y 14
+#   meet the condition because 21 = 7+14. The output will be one of these two messages: “Condition is
+#   met” or “Condition is not met”.
+
+# ------------------------------------------------------------------------------------------------------
+# Solution:
+a = float(input("Number 1: "))
+b = float(input("Number 2: "))
+c = float(input("Number 3: "))
+if a == b + c or b == a + c or c == a + b:
+    print("Condition is met")
+else:
+    print("Condition is not met")
